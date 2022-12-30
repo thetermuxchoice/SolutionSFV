07c9592f6af346d73c6c325f10d374074437b1d3
DESKTOP-6L08L2B\MystT <toimaxmermer12345@gmail.com>
2022-12-29 22:48:04 -0600

    update

--- js/services.js
+++ js/services.js
@@ -2,7 +2,7 @@ const menu = document.getElementById("pop-menu");
 const iframe = document.getElementById("child-conteiner");
 
 menu.addEventListener("click", function (event) {
-  let url = "/projects/" + event.target.slot + ".html";
+  let url = "projects/" + event.target.slot + ".html";
   iframe.src = url;
   iframe.classList.remove("reveal");
   iframe.offsetWidth;
