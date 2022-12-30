892e3691d1c583cc808d470fa07158753e841d02
DESKTOP-6L08L2B\MystT <toimaxmermer12345@gmail.com>
2022-12-29 22:20:38 -0600

    update

--- js/services.js
+++ js/services.js
@@ -1,11 +1,10 @@
 const menu = document.getElementById("pop-menu");
 const iframe = document.getElementById("child-conteiner");
 
-
-menu.addEventListener("click", function(event){
-    let url =    "projects/" + event.target.slot + ".html";
-    iframe.src = url;
-    iframe.classList.remove('reveal')
-    iframe.offsetWidth;
-    iframe.classList.add('reveal');
-})
+menu.addEventListener("click", function (event) {
+  let url = "projects/" + event.target.slot + ".html";
+  iframe.src = url;
+  iframe.classList.remove("reveal");
+  iframe.offsetWidth;
+  iframe.classList.add("reveal");
+});
